0cf416da06880485cd3f8e9bba2d0a982d27055c
Bjorn Boe <bvboe@pobox.com>
2026-04-23 11:38:28 -0400

    Graceful shutdown of app and rename desployment script

--- services/banking-app/worker/app.py
+++ services/banking-app/worker/app.py
@@ -1,20 +1,24 @@
 # app.py
-import time
+import time, signal, sys
 
 import pandas as pd
 
+signal.signal(signal.SIGTERM, lambda *_: sys.exit(0))
 
-def run_once() -> None:
+def run_once(counter) -> None:
     df = pd.DataFrame(
         {
             "a": [1, 2, 3, 4],
             "b": [10, 20, 30, 40],
+            "counter": counter
         }
     )
     print(df.mean(), flush=True)
 
 
 if __name__ == "__main__":
+    counter = 0
     while True:
-        run_once()
-        time.sleep(5)
+        run_once(counter)
+        counter = counter + 1
+        time.sleep(10)
